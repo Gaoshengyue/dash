91638aa0035b9f167389e690121d5dd0f194ed46
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2020-01-10 19:46:40 -0500

    feat: disable cookie access under restricted sandboxes
    When dash is embedded into an iframe with a sandbox attribute that only has allow-scripts, cookie access is disabled and dash fails to load. As such, we need to restrict our cookie usage by disabling functionality.
    
    This patch removes the disabled functionality in a graceful manner, allowing dash to load in very restricted iframes.

--- dash-renderer/src/AccessDenied.react.js
+++ dash-renderer/src/AccessDenied.react.js
@@ -28,9 +28,11 @@ function AccessDenied(props) {
             <a
                 style={styles.base.a}
                 onClick={() => {
-                    document.cookie =
-                        `${constants.OAUTH_COOKIE_NAME}=; ` +
-                        'expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+                    try {
+                        document.cookie =
+                            `${constants.OAUTH_COOKIE_NAME}=; ` +
+                            'expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+                    } catch (e) {}
                     window.location.reload(true);
                 }}
             >
